de73fdd478378df982bfacf12131efa61c1e12a8
code-mkumar <codehunting2k24@gmail.com>
2024-12-12 11:26:51 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -95,12 +95,12 @@ def write_files(data, filename="data.txt"):
         # Check if the file exists
         if not os.path.exists(filename):
             # If the file doesn't exist, create it and write the data
-            with open(filename, "w") as file:
+            with open("data.txt", "w") as file:
                 file.write(json.dumps(data) + "\n")
             print("File created and data written successfully.")
         else:
             # If the file exists, append the data
-            with open(filename, "a") as file:
+            with open("data.txt", "+a") as file:
                 file.write(json.dumps(data) + "\n")
             print("Data appended successfully.")
     except Exception as e:
