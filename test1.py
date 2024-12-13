72c0201d9068f6618bff8bd3ca25e9cb4b9af074
code-mkumar <codehunting2k24@gmail.com>
2024-12-13 12:20:10 +0530

    Update test1.py
    nijbniubniouuhn9u

--- test1.py
+++ test1.py
@@ -94,8 +94,8 @@ def guest_page():
     # Initialize session state
     if 'qa_list' not in st.session_state:
         st.session_state.qa_list = []
+    st.write("helo")
     with st.sidebar:
-
         if st.button("Go to Login"):
             st.session_state.page = "login"
         for qa in reversed(st.session_state.qa_list):
